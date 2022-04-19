e6807d6ef50daa781124a799b4099433eeb32efb
Costa Huang <costa.huang@outlook.com>
2022-04-18 22:58:12 -0400

    Allow the user to just specify instance types

--- launcha/launcha.py
+++ launcha/launcha.py
@@ -11,6 +11,10 @@ import wandb
 
 import launcha
 
+# by default we suggest using 80% of the available memory
+# when submitting the job (otherwise it might fails in our experience)
+MEMORY_USAGE_FRACTION = 0.8
+
 
 def parse_args():
     # fmt: off
@@ -28,11 +32,11 @@ def parse_args():
         help='the wandb key. If not provided, the script will try to read from `netrc`')
 
     # AWS Batch args for experiment submission
-    parser.add_argument('--job-queue', type=str, default="m6gd-medium",
-        help='the name of the job queue')
-    parser.add_argument('--num-vcpu', type=int, default=1,
+    parser.add_argument('--instance-type', type=str, default="g4dn.xlarge",
+        help='the instance type name (e.g., g4dn.xlarge)')
+    parser.add_argument('--num-vcpu', type=int, default=0,
         help='number of vcpu per experiment')
-    parser.add_argument('--num-memory', type=int, default=2000,
+    parser.add_argument('--num-memory', type=int, default=0,
         help='number of memory (MB) per experiment')
     parser.add_argument('--num-gpu', type=int, default=0,
         help='number of gpu per experiment')
@@ -53,6 +57,95 @@ def parse_args():
     return args
 
 
+def recommend_specs(instance_type: str):
+    ec2_client = boto3.client("ec2")
+    specs = ec2_client.describe_instance_types(InstanceTypes=[instance_type])["InstanceTypes"][0]
+    num_vcpu = specs["VCpuInfo"]["DefaultVCpus"]
+    num_memory = int(specs["MemoryInfo"]["SizeInMiB"] * MEMORY_USAGE_FRACTION)
+    gpu_count = 0
+    if "GpuInfo" in specs:
+        gpus_specs = specs["GpuInfo"]["Gpus"]
+        for gpus_spec in gpus_specs:
+            gpu_count += gpus_spec["Count"]
+    print(f"Recommended usage for {instance_type}: {num_vcpu} vcpu, {num_memory} MB memory, {gpu_count} gpu")
+    return num_vcpu, num_memory, gpu_count
+
+
+def submit_aws_job(
+    docker_tag: str,
+    run_command: str,
+    instance_type: str,
+    num_vcpu: int = 0,
+    num_memory: int = 0,
+    num_gpu: int = 0,
+    num_hours: float = 120.0,
+    num_retries: int = 1,
+    environment_variables: dict[str, str] = []
+):
+    recommended_num_vcpu, recommended_num_memory, recommended_gpu_count = recommend_specs(instance_type)
+    if num_vcpu == 0:
+        num_vcpu = recommended_num_vcpu
+    if num_memory == 0:
+        num_memory = recommended_num_memory
+    if num_gpu == 0:
+        num_gpu = recommended_gpu_count
+
+    batch_client = boto3.client("batch")
+    job_name = docker_tag.replace(":", "").replace("/", "_").replace(" ", "").replace("-", "_") + str(int(time.time()))
+    resources_requirements = []
+    if num_gpu:
+        resources_requirements = [
+            {"value": str(num_gpu), "type": "GPU"},
+        ]
+    try:
+        job_def_name = docker_tag.replace(":", "_").replace("/", "_")
+        batch_client.register_job_definition(
+            jobDefinitionName=job_def_name,
+            type="container",
+            containerProperties={
+                "image": docker_tag,
+                "vcpus": num_vcpu,
+                "memory": num_memory,
+                "command": [
+                    "/bin/bash",
+                ],
+            },
+        )
+        response = batch_client.submit_job(
+            jobName=job_name,
+            jobQueue=instance_type,
+            jobDefinition=job_def_name,
+            containerOverrides={
+                "vcpus": num_vcpu,
+                "memory": num_memory,
+                "command": ["/bin/bash", "-c", run_command],
+                "environment": environment_variables,
+                "resourceRequirements": resources_requirements,
+            },
+            retryStrategy={"attempts": num_retries},
+            timeout={"attemptDurationSeconds": int(num_hours * 60 * 60)},
+        )
+        if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+            print(response)
+            raise Exception("jobs submit failure")
+    except Exception as e:
+        print(e)
+    finally:
+        response = batch_client.deregister_job_definition(jobDefinition=job_def_name)
+        if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+            print(response)
+            raise Exception("jobs submit failure")
+
+
+def build_docker(push: bool, archs: str, docker_tag: str):
+    output_type_str = "--output=type=registry" if push else "--output=type=docker"
+    subprocess.run(
+        f"docker buildx build {output_type_str} --platform {archs} -t {docker_tag} .",
+        shell=True,
+        check=True,
+    )
+
+
 def main():
     args = parse_args()
     if args.subcommand == "init":
@@ -68,12 +161,7 @@ The computing environments' setup is free of charge. You will only be billed whe
         return
 
     if args.build:
-        output_type_str = "--output=type=registry" if args.push else "--output=type=docker"
-        subprocess.run(
-            f"docker buildx build {output_type_str} --platform {args.archs} -t {args.docker_tag} .",
-            shell=True,
-            check=True,
-        )
+        build_docker(args.push, args.archs, args.docker_tag)
 
     if not args.wandb_key:
         try:
@@ -82,69 +170,31 @@ The computing environments' setup is free of charge. You will only be billed whe
             pass
     assert len(args.wandb_key) > 0, "you have not logged into W&B; try do `wandb login`"
 
-    final_run_cmds = [args.command]
 
-    for final_run_cmd in final_run_cmds:
-        run_command = (
-            f"docker run -d -e WANDB_API_KEY={args.wandb_key} {args.docker_tag} "
-            + '/bin/bash -c "'
-            + final_run_cmd
-            + '"'
-            + "\n"
-        )
-        print(run_command)
+    run_command = (
+        f"docker run -d -e WANDB_API_KEY={args.wandb_key} {args.docker_tag} "
+        + '/bin/bash -c "'
+        + args.command
+        + '"'
+        + "\n"
+    )
+    print(run_command)
 
     # submit jobs
     if args.provider == "aws":
-        client = boto3.client("batch")
-        for final_run_cmd in final_run_cmds:
-            job_name = args.docker_tag.replace(":", "").replace("/", "_").replace(" ", "").replace("-", "_") + str(
-                int(time.time())
-            )
-            resources_requirements = []
-            if args.num_gpu:
-                resources_requirements = [
-                    {"value": str(args.num_gpu), "type": "GPU"},
-                ]
-            try:
-                job_def_name = args.docker_tag.replace(":", "_").replace("/", "_")
-                client.register_job_definition(
-                    jobDefinitionName=job_def_name,
-                    type="container",
-                    containerProperties={
-                        "image": args.docker_tag,
-                        "vcpus": args.num_vcpu,
-                        "memory": args.num_memory,
-                        "command": [
-                            "/bin/bash",
-                        ],
-                    },
-                )
-                response = client.submit_job(
-                    jobName=job_name,
-                    jobQueue=args.job_queue,
-                    jobDefinition=job_def_name,
-                    containerOverrides={
-                        "vcpus": args.num_vcpu,
-                        "memory": args.num_memory,
-                        "command": ["/bin/bash", "-c", final_run_cmd],
-                        "environment": [
-                            {"name": "WANDB_API_KEY", "value": args.wandb_key},
-                            {"name": "WANDB_RESUME", "value": "allow"},
-                            {"name": "WANDB_RUN_ID", "value": wandb.util.generate_id()},
-                        ],
-                        "resourceRequirements": resources_requirements,
-                    },
-                    retryStrategy={"attempts": args.aws_num_retries},
-                    timeout={"attemptDurationSeconds": int(args.num_hours * 60 * 60)},
-                )
-                if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
-                    print(response)
-                    raise Exception("jobs submit failure")
-            except Exception as e:
-                print(e)
-            finally:
-                response = client.deregister_job_definition(jobDefinition=job_def_name)
-                if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
-                    print(response)
-                    raise Exception("jobs submit failure")
+        submit_aws_job(
+            args.docker_tag,
+            run_command,
+            args.instance_type,
+            num_vcpu=args.num_vcpu,
+            num_memory=args.num_memory,
+            num_gpu=args.num_gpu,
+            num_hours=args.num_hours,
+            num_retries=args.aws_num_retries,
+            environment_variables=[
+                {"name": "WANDB_API_KEY", "value": args.wandb_key},
+                {"name": "WANDB_RESUME", "value": "allow"},
+                {"name": "WANDB_RUN_ID", "value": wandb.util.generate_id()},
+            ]
+        )
+
